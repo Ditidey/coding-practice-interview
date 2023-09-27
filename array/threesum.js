cb51a83f6019e079001dab7e2956a78e86562ba2
Diti Rani Dey <ditidey2017@gmail.com>
2023-09-27 20:53:09 +0100

    three sum added

--- array/threesum.js
+++ array/threesum.js
@@ -1,6 +1,27 @@
 function threeNumberSum(array, targetSum) {
-    // Write your code here.
-  }
+    array.sort((a, b) => a - b)
+    const triplets = [];
+    for (let i = 0; i < array.length - 2; i++) {
+        let left = i + 1;
+        let right = array.length - 1
+        while (left < right) {
+            let currentSum = array[i] + array[left] + array[right];
+            if (currentSum == targetSum) {
+                triplets.push([array[i], array[left], array[right]])
+                right--;
+                left++;
+            }
+            else if (currentSum > targetSum) {
+                right--;
+            }
+            else if (currentSum < targetSum) {
+                left++;
+            }
+        }
+    }
 
-  const result =  threeNumberSum([12, 1, -8, 4, 3, 6, 5, -6, 2], 0)
-  console.log(result)
+    return triplets;
+}
+
+const result = threeNumberSum([12, 1, -8, 4, 3, 6, 5, -6, 2], 0)
+console.log(result)
